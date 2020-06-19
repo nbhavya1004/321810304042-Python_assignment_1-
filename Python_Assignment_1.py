a8569deffbe246f7b3dbe5b83c254590a5f70529
nbhavya1004 <67138769+nbhavya1004@users.noreply.github.com>
2020-06-19 11:27:36 +0530

    Add files via upload

---
+++ Python_Assignment_1.py
@@ -0,0 +1,124 @@
+# python_assignment_1
+
+# Bhavyasree N - 321810304042
+
+
+# 1.to design Simple Calculator
+x,y=10,10           #Addition
+print(x+y)
+
+x=15
+y=15
+z=x+y
+print(z)
+
+x,y=20,10           #Subtraction
+print(x-y)
+
+x,y=5,3               #Multiplication
+print(x*y)
+
+x,y=60,30          #Division
+print(x/y)
+
+x,y=100,15        #Modulus
+print(x%y)
+
+x=4**4               #Exponentiation
+print(x)
+
+x,y=60,30            #Floor Division
+print(x//y)
+
+
+# 2.Simple Interest(si)
+p=150
+t=14
+r=3
+si=p*t*r/100
+print(si)
+
+
+# 3.Area of a Circle
+PI=3.14
+r=5
+Area=PI*r*r
+print(Area)
+
+
+# 4.Area of Triangle
+a=10
+b=4
+Area=(a*b/2)
+print(Area)
+
+
+# 5.Temperature in Celsius to Fahrenheit
+celsius=22.5
+fahrenheit=(celsius*1.8)+32
+print(fahrenheit)
+
+
+# 6.Area of Rectangle
+w=5
+l=8
+Area=w*l
+print(Area)
+
+
+# 7.Perimeter of a Square
+a=20
+P=4*a
+print(P)
+
+
+# 8.Circumference of a Circle
+PI=3.14
+r=4
+C=2*PI*r
+print(C)
+
+
+# 9.Swapping of Two numbers
+x,y=10,20
+temp=x
+x=y
+y=temp
+print(x,y)
+
+x=input('Enter value of x : ')
+y=input('Enter value of y : ')
+x,y=y,x
+print(x)
+print(y)
+
+
+                               # python_assignment_1
+                                   
+                               #   Nanjundareddy Bhavyasree
+                               #  2nd year
+                               #  Section : D
+                               #    Roll no..: 321810304042
+                                     
+____________________________________
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
